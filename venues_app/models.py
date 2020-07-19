7b3c816a835b173e4454ebdaa9789fa301a1f9db
rosz <anna.roszman@gmail.com>
2020-07-19 22:22:18 +0200

    WIP: edit models

--- venues_app/models.py
+++ venues_app/models.py
@@ -6,9 +6,9 @@ class Venue(models.Model):
     name = models.CharField(max_length=100, verbose_name='nazwa')
     description = models.TextField(verbose_name='opis')
     address = models.TextField(verbose_name='adres')
-    # image = models.ImageField()
-    avg_rating = models.Aggregate()
-    created_at = models.DateTimeField(verbose_name='data dodania', default=timezone.now())
+    image = models.ImageField(verbose_name='obraz', upload_to='media', help_text="Dodaj obrazek o wymiarach 200x200px")
+    avg_rating = models.TextField(verbose_name='średnia ocen', default=0)
+    created_at = models.DateTimeField(verbose_name='data dodania', default=timezone.now)
 
     def __str__(self):
         return f'{self.name} @ {self.address}'
@@ -23,6 +23,10 @@ class Venue(models.Model):
 
 class User(models.Model):
     login = models.CharField(max_length=100, verbose_name='nazwa użytkownika')
+    created_at = models.DateTimeField(verbose_name='data dodania', default=timezone.now)
+
+    def __str__(self):
+        return f'{self.login}'
 
     class Meta:
         verbose_name = 'Użytkownik'
@@ -30,11 +34,14 @@ class User(models.Model):
 
 
 class Rating(models.Model):
-    rate = models.IntegerChoices
-    user = models.ForeignKey(to=User, on_delete=models.DO_NOTHING)
-    venue = models.ForeignKey(to=Venue, on_delete=models.DO_NOTHING)
+    rate = models.IntegerField(default=0)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    venue = models.ForeignKey(to=Venue, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}, {self.venue}, {self.rate}'
 
     class Meta:
         unique_together = ['user', 'venue']
-        verbose_name = 'Ocena'
-        verbose_name_plural = 'Oceny'
+        verbose_name = 'Ocena użytkownika'
+        verbose_name_plural = 'Oceny użytkowników'
