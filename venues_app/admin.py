708f349e0adb2c72bca843cc438f6ff17d9a819e
rosz <anna.roszman@gmail.com>
2020-07-19 22:23:36 +0200

    WIP: register models in admin

--- venues_app/admin.py
+++ venues_app/admin.py
@@ -1,12 +1,43 @@
 from django.contrib import admin
-from .models import Venue, User
+from .models import Venue, User, Rating
 
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
-    fields = ('name', 'description', 'address')
+    ordering = ('name',)
+    fields = ('name',  'created_at', 'avg_rating', 'address', 'image', 'description',)
+    search_fields = ('name',)
+    readonly_fields = ('avg_rating', 'created_at')
+    list_per_page = 5
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    fields = ('login',)
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     ordering = ('created_at',)
+#     fields = ('login', 'created_at',)
+#     search_fields = ('login',)
+#     readonly_fields = ('login', 'created_at',)
+#     list_per_page = 5
+#
+#     def has_delete_permission(self, request, obj=None):
+#         return False
+#
+#     def has_change_permission(self, request, obj=None):
+#         return False
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'venue', 'rate')
+    search_fields = ('user', 'venue')
+    ordering = ('user',)
+    readonly_fields = ('user', 'venue', 'rate')
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+
